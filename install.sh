d12f4b6d61c818d2dee7e8f265106d7d49294827
gfgfgfh <vcore1@yandex.ru>
2017-03-28 06:48:49 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+MASTER_SITES=http://de.archive.ubuntu.com/ubuntu/
+UBUNTU_VERSION=(sed -n 1p config)
+ARCH=(sed -n 2p config)
+ubuntu=ubuntu_$UBUNTU_VERSION"_"$ARCH
+tar=tar_$UBUNTU_VERSION"_"$ARCH
+deb=deb_$UBUNTU_VERSION"_"$ARCH
