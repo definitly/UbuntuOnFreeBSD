195bf80fd2a8999f2c3e2e32e8581b9403349ddd
gfgfgfh <vcore1@yandex.ru>
2017-03-28 10:35:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MASTER_SITES=http://de.archive.ubuntu.com/ubuntu/
+MASTER_SITES=http://de.archive.ubuntu.com/ubuntu/s
 UBUNTU_VERSION=(sed -n 1p config)
 ARCH=(sed -n 2p config)
 ubuntu=ubuntu_$UBUNTU_VERSION"_"$ARCH
@@ -13,3 +13,8 @@ if ! [ "$ARCH" == "i386" -o "$ARCH" =="amd64" ]; then
     exit 1;
                  fi
 
+if ! [ "$UBUNTU_VERSION"  == "14.04" -o "$UBUNTU_VERSION" == "16.10" ]; then
+
+   echo "Choose 14.04 or 16.10"
+
+          exit 1;
