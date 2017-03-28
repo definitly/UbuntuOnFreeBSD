4130f9d54589cb78b9f9c64df63f034146618470
gfgfgfh <vcore1@yandex.ru>
2017-03-28 06:21:07 +0100

    Update install_amd64.sh

--- install_amd64.sh
+++ install_amd64.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 MASTER_SITES=http://de.archive.ubuntu.com/ubuntu/
-UBUNTU_VERSION=14.04
-ARCH=amd64
+UBUNTU_VERSION=(sed -n 1p config)
+ARCH=(sed -n 2p config)
 ubuntu=ubuntu_$UBUNTU_VERSION"_"$ARCH
 tar=tar_$UBUNTU_VERSION"_"$ARCH
 deb=deb_$UBUNTU_VERSION"_"$ARCH
