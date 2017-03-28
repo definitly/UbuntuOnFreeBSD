695c8d87316b38c40c810570f7fe44e738029d9f
gfgfgfh <vcore1@yandex.ru>
2017-03-28 09:17:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-MASTER_SITES=http://de.archive.ubuntu.com/ubuntu/
+MASTER_SITES=http://de.archive.ubuntu.com/ubuntu/w
 UBUNTU_VERSION=(sed -n 1p config)
 ARCH=(sed -n 2p config)
 ubuntu=ubuntu_$UBUNTU_VERSION"_"$ARCH
 tar=tar_$UBUNTU_VERSION"_"$ARCH
 deb=deb_$UBUNTU_VERSION"_"$ARCH
 
-if [ "$
+if ! [ "$ARCH" == "i386" -o "$ARCH" =="amd64" ]; then
+
+
+                 fi
 
